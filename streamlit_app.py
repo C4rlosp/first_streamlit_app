887eb282d5bab80af90bc653af176f975b49e214
Carlos <55120053+C4rlosp@users.noreply.github.com>
2024-01-26 00:07:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,13 +14,12 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #importando una lista de frutas desde AWS S3
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-#permitiendo elegir de la lista las frutas que ellos quieran incluir
-#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-#display the table on the page
+#permitiendo elegir de la lista las frutas que ellos quieran incluir
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
+#display the table on the page
 streamlit.dataframe(my_fruit_list)
 
 
