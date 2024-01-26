881365918cf22dd6a8418ee4cc1da71b5fd9b1a3
Carlos <55120053+C4rlosp@users.noreply.github.com>
2024-01-26 00:02:03 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,21 @@
 
 import streamlit
-
+#agregando titulo a la app
 streamlit.title('My parents New healthy Diner')
 
+#agregando el menu
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+#agregando un encabezado
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
+#importando una lista de frutas desde AWS S3
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+
+#permitiendo elegir de la lista las frutas que ellos quieran incluir
+streamlit.multiselector("Pick some fruits:", list(my_fruit_list.index))
+#display the table on the page
